a96d4ac5b252d8a86115e3425378da9175ddc6b9
43ntropy <val.ferrandino@gmail.com>
2025-09-07 02:09:59 +0000

    ⬆️ Update CockroachDB to 25.3.1

--- src/Enviroment.ts
+++ src/Enviroment.ts
@@ -1,4 +1,4 @@
 export enum Environment {
-    COCKROACH_VERSION = "25.3.0",
+    COCKROACH_VERSION = "25.3.1",
     COCKROACH_DOWNLOAD = `https://binaries.cockroachdb.com/cockroach-v${Environment.COCKROACH_VERSION}.linux-amd64.tgz`
 }
