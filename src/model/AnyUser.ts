fb08820839fa250b5b43f2c2397bdf2cbf17714a
43ntropy <val.ferrandino@gmail.com>
2025-09-07 02:03:04 +0000

    🚧 AnyUser get and create basic implementation

--- src/model/AnyUser.ts
+++ src/model/AnyUser.ts
@@ -1,25 +1,77 @@
-import { UUID } from "crypto";
+import { randomUUID, UUID } from "crypto";
 import { Model } from "./Model";
 
 interface AnyUserBrand {
-    readonly __anyobject: unique symbol;
+    readonly __anyuser: unique symbol;
 }
 
 export type AnyUserUUID = UUID & AnyUserBrand;
 
 export class AnyUser extends Model {
 
-    private readonly uuid: AnyUserUUID;
+    public readonly uuid: AnyUserUUID;
 
-    private constructor() {
+    public displayName: string;
+
+    private constructor(
+        uuid: AnyUserUUID,
+        displayName: string
+    ) {
         super();
-        this.uuid = crypto.randomUUID() as AnyUserUUID;
+        this.uuid = uuid;
+        this.displayName = displayName;
+        // ! Tmp to avoid unused variable error
         this.uuid;
+        this.displayName;
     }
 
+    public static async create(
+        displayName: string = ""
+    ): Promise<AnyUser> {
+
+        const queryResult = await Model.cockroachClient.query(`
+            INSERT INTO 
+            public.anyuser ("uuid", "displayname")
+            VALUES 
+            ($1, $2)
+            RETURNING "uuid", "displayname"
+        `, [
+            randomUUID(),
+            displayName
+        ]);
+
+        if (queryResult.rowCount == 0)
+            throw new Error("AnyUser creation failed");
+
+        return new AnyUser(
+            queryResult.rows[0].uuid,
+            queryResult.rows[0].displayname
+        );
+
+    }
+
+    public static async get(uuid: AnyUserUUID): Promise<AnyUser> {
+
+        const queryResult = await Model.cockroachClient.query(`
+            SELECT "uuid", "displayname"
+            FROM public.anyuser
+            WHERE "uuid" = $1
+        `, [
+            uuid
+        ]);
+
+        if (queryResult.rowCount == 0)
+            throw new Error("AnyUser not found");
+        
+        return new AnyUser(
+            queryResult.rows[0].uuid,
+            queryResult.rows[0].displayname
+        );
+
+    }
 
     protected override save(): void {
         throw new Error("Method not implemented.");
     }
-    
+
 }
